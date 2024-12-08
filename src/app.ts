15ab26d6a45c930478fec28af7b634a75c20dc74
devJayed <jayedhossain.dev@gmail.com>
2024-12-08 09:52:45 +0600

    root directory

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application} from 'express';
+import express, { Application, Request, Response} from 'express';
 import cors from 'cors';
 import { OrderRoutes, ProductRoutes } from './modules/bicycle/bicycle.route';
 const app: Application = express();
@@ -6,6 +6,15 @@ const app: Application = express();
 //parser
 app.use(express.json());
 app.use(cors());
+
+// Root route
+app.get('/', async(req: Request, res: Response) => {
+    res.status(200).json({
+      message: 'Welcome to BikeStore API Service!',
+      success: true,
+
+    });
+  });
 
 //application routes
 app.use('/api/v1/products', ProductRoutes);
